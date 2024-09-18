81686ddb497bcadf6b6ac20e3efea516543aa010
alexandertuna <alexander.tuna@gmail.com>
2024-09-17 21:45:23 -0700

    Trying to be a vectorized guy, not a for loop guy

--- scripts/convert_from_atlas.py
+++ scripts/convert_from_atlas.py
@@ -1,15 +1,22 @@
-from pads_ml.padpolygons import PadPolygons
+from pads_ml.padpolygons import Pads
 
 def main():
 
-    filename_i = "data/STGCPadTrigger.txt"
-    filename_o = "data/STGCPadTrigger.A05.txt"
+    # i/o
+    filename_i = "data/STGCPadTrigger.np.txt"
+    filename_o = "data/STGCPadTrigger.np.A05.txt"
 
-    with open(filename_o, "w") as fi:
-        all_pads = PadPolygons(filename_i)
-        for pad in all_pads:
-            if pad.wheel == 0 and pad.sector == 4:
-                fi.write(pad.line)
+    # get input pads
+    pads = Pads(filename_i, create_polygons=False)
+
+    # require wheel A, sector 5 (numbered from 1)
+    df_A05 = pads.df[
+        (pads.df["wheel"] == 0xA)
+        & (pads.df["sector"] == 4)
+    ]
+
+    # write to disk
+    df_A05.to_csv(filename_o, sep=' ', index=False)
 
 
 if __name__ == "__main__":
--- src/pads_ml/padpolygons.py
+++ src/pads_ml/padpolygons.py
@@ -1,105 +1,32 @@
-from dataclasses import dataclass
+import pandas as pd
+import geopandas as gpd
 from shapely.geometry import Polygon
 
-
-def is_valid(line: str) -> bool:
-    return line.startswith("Pad")
-
-
-class PadPolygons:
+class Pads:
     """
-    PadPolygons is a container class for PadPolygons
+    Pads holds a pandas dataframe of pad geometry
     """
-    def __init__(self, filename: str) -> None:
+    def __init__(self,
+                 filename: str,
+                 create_polygons: bool = True,
+                 ) -> None:
+
         self.filename = filename
-        with open(self.filename) as fi:
-            self.pads = tuple(
-                PadPolygon(*PadLine(line).data) for line in fi if is_valid(line)
-            )
+        self.df = pd.read_csv(self.filename, sep="\s+")
 
-    def __iter__(self):
-        return iter(self.pads)
+        hexify = lambda x: int(x, 16)
+        self.df["wheel"] = self.df["wheel_hex"].apply(hexify)
+        self.df["id"] = self.df["id_hex"].apply(hexify)
 
+        if create_polygons:
+            def create_polygon(row):
+                points = [
+                    (row["x0"], row["y0"]),
+                    (row["x1"], row["y1"]),
+                    (row["x2"], row["y2"]),
+                    (row["x3"], row["y3"]),
+                ]
+                return Polygon(points)
 
-@dataclass()
-class PadPolygon:
-    """
-    PadPolygon is a dataclass for holding attributes which describe a pad.
-    """
-    line: str
-    id: int
-    wheel: int
-    sector: int
-    pfeb: int
-    tds_channel: int
-    pad_channel: int
-    x0: float
-    y0: float
-    z0: float
-    x1: float
-    y1: float
-    z1: float
-    x2: float
-    y2: float
-    z2: float
-    x3: float
-    y3: float
-    z3: float
-
-    def __post_init__(self):
-        self.polygon = Polygon([
-            (self.x0, self.y0),
-            (self.x1, self.y1),
-            (self.x3, self.y3),
-            (self.x2, self.y2),
-        ])
-
-        # All the zs should be very similar
-        zs = (self.z0, self.z1, self.z2, self.z3)
-        if abs(min(zs) - max(zs)) > 10:
-            raise Exception(f"Weird z values: {zs}")
-
-        self.z = sum(zs) / len(zs)
-
-
-class PadLine:
-    """
-    PadLine takes a line of text as input, and converts it to a list of attributes
-      which describe a pad.
-    """
-    def __init__(self, line: str) -> None:
-        if not is_valid(line):
-            raise Exception("Cannot unpack invalid PadLine")
-        self.line = line
-        self.data = self.unpack()
-
-    def is_valid(self) -> bool:
-        return self.line.startswith("Pad")
-
-    def unpack(self) -> tuple:
-        (key, id, whl, sec, pfeb, tdsch, padch,
-         x0, y0, z0, x1, y1, z1,
-         x2, y2, z2, x3, y3, z3) = self.line.split()
-
-        return (
-            self.line,
-            int(id, base=16),
-            0 if whl=="A" else 1,
-            int(sec),
-            int(pfeb),
-            int(tdsch),
-            int(padch),
-            float(x0),
-            float(y0),
-            float(z0),
-            float(x1),
-            float(y1),
-            float(z1),
-            float(x2),
-            float(y2),
-            float(z2),
-            float(x3),
-            float(y3),
-            float(z3),
-        )
-
+            self.df["geometry"] = self.df.apply(create_polygon, axis=1)
+            self.gdf = gpd.GeoDataFrame(self.df, geometry="geometry")

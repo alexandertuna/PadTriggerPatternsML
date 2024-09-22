b2aac334afdea4e402e484f82aaff71192d11851
alexandertuna <alexander.tuna@gmail.com>
2024-09-21 22:14:06 -0700

    Simplify imports. Dont bother saving lines/pads to self. fillna(-1)

--- src/pads_ml/traverser.py
+++ src/pads_ml/traverser.py
@@ -1,31 +1,23 @@
-import numpy as np
 import pandas as pd
 import geopandas as gpd
-from shapely.geometry import Point
-from typing import Any
 
 from . import constants
+from .pads import Pads
+from .lines import Lines
 
 class Traverser:
 
     def __init__(
         self,
-        lines: pd.DataFrame,
-        pads: Any,
+        lines: Lines,
+        pads: Pads,
     ):
-        self.lines = lines
-        self.pads = pads
 
         # For each layer, find which pad (if any) contains the line
         self.df = pd.DataFrame()
         for layer in range(constants.LAYERS):
-            lines_gdf = gpd.GeoDataFrame(self.lines, geometry=f"point_{layer}")
-            pads_gdf = gpd.GeoDataFrame(self.pads.layer[layer], geometry="geometry")
+            lines_gdf = gpd.GeoDataFrame(lines.df, geometry=f"point_{layer}")
+            pads_gdf = gpd.GeoDataFrame(pads.layer[layer], geometry="geometry")
             joined = lines_gdf.sjoin(pads_gdf, how="left", predicate="within")
-            self.df[f"intersecting_pad_{layer}"] = joined["index_right"]
-
-        print(self.df)
-
-        # for layer in range(constants.LAYERS):
-        #     self.df[f"pad_{layer}"] = self.pads[self.pads["layer"] == layer].apply(self._find_pad, axis=1)
+            self.df[f"intersecting_pad_{layer}"] = joined["index_right"].fillna(-1).astype(int)
 

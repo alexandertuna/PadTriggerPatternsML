a06cd9a67289f8fc206b93857e517498697909e2
alexandertuna <alexander.tuna@gmail.com>
2024-09-22 11:24:10 -0700

    Make the output filename more sensible

--- scripts/generate.py
+++ scripts/generate.py
@@ -3,16 +3,18 @@ from pads_ml.generator import SignalGenerator, NoiseGenerator
 
 def main():
     now = time.strftime("%Y_%m_%d_%H_%M_%S")
-    print(now)
     num = 10000
+
+    print("Generating signal ...")
     signal = SignalGenerator(num, "data/STGCPadTrigger.np.A05.txt")
     print(signal.df)
 
+    print("Generating noise ...")
     noise = NoiseGenerator(num, "data/STGCPadTrigger.np.A05.txt")
     print(noise.df)
 
-    signal.df.to_parquet(f"signal.parquet")
-    noise.df.to_parquet(f"noise.parquet")
+    signal.df.to_parquet(f"signal.{now}.{num}.parquet")
+    noise.df.to_parquet(f"noise.{now}.{num}.parquet")
 
 if __name__ == "__main__":
     main()

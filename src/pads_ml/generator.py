77fb610173a6f4557f9bcd1dea9f23837bcd1d31
alexandertuna <alexander.tuna@gmail.com>
2024-09-21 22:13:17 -0700

    Simplify imports. remove per-layer attrs from generator df. merge lines with intersecting pads

--- src/pads_ml/generator.py
+++ src/pads_ml/generator.py
@@ -1,9 +1,24 @@
-from pads_ml.pads import Pads
-from pads_ml.lines import Lines
-from pads_ml.traverser import Traverser
+from . import constants
+from .pads import Pads
+from .lines import Lines
+from .traverser import Traverser
 
 class Generator:
-    def __init__(self, num: int):
-        self.lines = Lines(num)
-        self.pads = Pads("data/STGCPadTrigger.np.A05.txt")
-        self.traverser = Traverser(self.lines.df, self.pads)
+    def __init__(self, num: int, pads_filename: str):
+        lines = Lines(num)
+        pads = Pads(pads_filename)
+        traverser = Traverser(lines, pads)
+
+        if len(lines.df) != len(traverser.df):
+            raise ValueError(f"Lines and Traverser have different lengths: {len(lines.df)} != {len(traverser.df)}")
+
+        # drop the per-layer columns from lines
+        columns = [f"{coord}_{layer}" for coord in ["x", "y", "quad", "point"] for layer in range(constants.LAYERS)]
+        lines.df = lines.df.drop(columns=columns)
+
+        self.df = lines.df.merge(
+            traverser.df,
+            how="left",
+            left_index=True,
+            right_index=True,
+        )

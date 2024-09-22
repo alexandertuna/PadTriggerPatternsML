b2e6b37bda07bd6148eddfe6fe859aebd5602040
alexandertuna <alexander.tuna@gmail.com>
2024-09-21 22:12:43 -0700

    Try writing to file

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,11 +1,16 @@
+import time
 from pads_ml.generator import Generator
 
 def main():
-    num = 1000
-    gen = Generator(num)
+    now = time.strftime("%Y_%m_%d_%H_%M_%S")
+    print(now)
+    num = 100000
+    gen = Generator(num, "data/STGCPadTrigger.np.A05.txt")
     # print(gen.lines.df)
     # print(gen.pads.df)
-    # print(gen.traverser.df)
+    print(gen.df)
+
+    gen.df.to_parquet(f"tmp.parquet")
 
 if __name__ == "__main__":
     main()

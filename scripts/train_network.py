1d4c51ce6958f8dafa161e2beebfaea66dca0bea
alexandertuna <alexander.tuna@gmail.com>
2024-09-23 21:27:29 -0700

    Pass list of files

--- scripts/train_network.py
+++ scripts/train_network.py
@@ -14,17 +14,21 @@ def main():
     # noise_name = Path("noise.2024_09_22_17_44_41.1000000.parquet")
     # signal_name = Path("signal.2024_09_22_17_43_09.100000.one_hot.npy")
     # noise_name = Path("noise.2024_09_22_17_43_09.100000.one_hot.npy")
-    features_name = Path("combined.2024_09_22_17_43_09.100000.one_hot.features.npy")
-    labels_name = Path("combined.2024_09_22_17_43_09.100000.one_hot.labels.npy")
+    # features_name = Path("combined.2024_09_22_17_43_09.100000.one_hot.features.npy")
+    # labels_name = Path("combined.2024_09_22_17_43_09.100000.one_hot.labels.npy")
 
-    logging.info(f"Opening {features_name} ...")
-    features = np.load(features_name)
-
-    logging.info(f"Opening {labels_name} ...")
-    labels = np.load(labels_name)
+    features_train_path = list(Path("train").glob("*features*npy"))
+    features_valid_path = list(Path("valid").glob("*features*npy"))
+    labels_train_path = list(Path("train").glob("*labels*npy"))
+    labels_valid_path = list(Path("valid").glob("*labels*npy"))
 
     logging.info(f"Creating model ...")
-    trainer = OneHotFullyConnectedTrainer(features, labels)
+    trainer = OneHotFullyConnectedTrainer(
+        features_train_path,
+        features_valid_path,
+        labels_train_path,
+        labels_valid_path,
+    )
 
     logging.info(f"Training model ...")
     trainer.train()

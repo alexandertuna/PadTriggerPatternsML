d98b9841437872b248c72ce761e56ecfd17e9c03
alexandertuna <alexander.tuna@gmail.com>
2024-09-23 21:28:00 -0700

    Simplify model for now

--- src/pads_ml/model.py
+++ src/pads_ml/model.py
@@ -12,14 +12,17 @@ class OneHotFullyConnected(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc_0 = nn.Linear(constants.PADS, 256)
-        self.fc_1 = nn.Linear(256, 128)
-        self.fc_2 = nn.Linear(128, 1)
+        self.fc_0 = nn.Linear(constants.PADS, 64)
+        self.fc_1 = nn.Linear(64, 1)
+        # self.fc_0 = nn.Linear(constants.PADS, 256)
+        # self.fc_1 = nn.Linear(256, 128)
+        # self.fc_2 = nn.Linear(128, 1)
         # print the number of parameters
         print(f"Model parameters: {sum(p.numel() for p in self.parameters())}")
 
     def forward(self, x):
         x = F.relu(self.fc_0(x))
-        x = F.relu(self.fc_1(x))
-        x = torch.sigmoid(self.fc_2(x))
+        x = torch.sigmoid(self.fc_1(x))
+        # x = F.relu(self.fc_1(x))
+        # x = torch.sigmoid(self.fc_2(x))
         return x

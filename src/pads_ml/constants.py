fc2bbb67f9ac4459875a356ad78c916b667f6b32
alexandertuna <alexander.tuna@gmail.com>
2024-09-24 19:32:07 -0700

    Adjusting constants to encompass the whole sector

--- src/pads_ml/constants.py
+++ src/pads_ml/constants.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-PHI_MIN = np.pi/2 - np.pi/16
-PHI_MAX = np.pi/2 + np.pi/16
-ETA_MIN = 1.3
-ETA_MAX = 2.0
+PHI_MIN = np.pi/2 - np.pi/12
+PHI_MAX = np.pi/2 + np.pi/12
+R_MIN = 800.0
+R_MAX = 5000.0
+ETA_MIN = 1.22
+ETA_MAX = 2.8
 
 PADS = 1739
 LAYERS = 8
@@ -21,11 +23,13 @@ ZS = [
     7809.0,
     7820.0,
 ]
+ZMID = sum(ZS) / len(ZS)
 
 RMAXS = [
     2260.0,
     3470.0,
-    4650.0,
+    # 4650.0,
+    5000.0,
 ]
 
 SECTOR_XMIN = -1030.0

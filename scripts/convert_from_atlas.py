6fc1b7fa7a528bf3c11d06eb313b8d2a030aaf26
alexandertuna <alexander.tuna@gmail.com>
2024-09-25 21:12:20 -0700

    add cl args and logging

--- scripts/convert_from_atlas.py
+++ scripts/convert_from_atlas.py
@@ -1,22 +1,36 @@
+import argparse
 from pads_ml.pads import Pads
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", help="Input file, whole wheel", required=True)
+    parser.add_argument("-o", help="Output file, A05 only", required=True)
+    return parser.parse_args()
+
 
 def main():
 
-    # i/o
-    filename_i = "data/STGCPadTrigger.np.txt"
-    filename_o = "data/STGCPadTrigger.np.A05.txt"
+    # CL args
+    ops = options()
 
     # get input pads
-    pads = Pads(filename_i, create_polygons=False)
+    logging.info(f"Reading pads from {ops.i}")
+    pads = Pads(ops.i, create_polygons=False)
 
     # require wheel A, sector 5 (numbered from 1)
+    logging.info("Selecting A05")
     df_A05 = pads.df[
         (pads.df["wheel"] == 0xA)
         & (pads.df["sector"] == 4)
     ]
 
     # write to disk
-    df_A05.to_csv(filename_o, sep=' ', index=False)
+    logging.info(f"Writing pads to {ops.o}")
+    df_A05.to_csv(ops.o, sep=' ', index=False)
 
 
 if __name__ == "__main__":

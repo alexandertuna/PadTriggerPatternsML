d6a6c7ddab4ab2a0fcf38890d66c7d444797d09d
alexandertuna <alexander.tuna@gmail.com>
2024-09-30 19:24:56 -0700

    Lets try a uniform distribution

--- pads_ml/lines.py
+++ pads_ml/lines.py
@@ -28,20 +28,43 @@ class Lines:
 
         self.num = num
         self.df = pd.DataFrame()
+        logger.info("Creating lines with randomly sampled parameters")
+
+        use_atlas = False
 
         # use ATLAS eta distribution
-        logger.info("Creating lines with randomly sampled parameters")
-        eta = np.random.normal(constants.ATLAS_ETA_MU, constants.ATLAS_ETA_SIGMA, 10 * self.num)
-        eta = eta[ (eta > constants.ETA_MIN) & (eta < constants.ETA_MAX) ][:self.num]
+        if use_atlas:
 
-        # Generate random r (mid) and phi
-        self.df["eta"] = eta
-        self.df["phi"] = np.random.uniform(constants.PHI_MIN, constants.PHI_MAX, self.num)
+            # generate and trim
+            eta = np.random.normal(constants.ATLAS_ETA_MU, constants.ATLAS_ETA_SIGMA, 10 * self.num)
+            eta = eta[ (eta > constants.ETA_MIN) & (eta < constants.ETA_MAX) ][:self.num]
 
-        # Calculate stuff
-        self.df["theta"] = 2 * np.arctan(np.exp(-self.df["eta"]))
+            # calculate
+            self.df["eta"] = eta
+            self.df["phi"] = np.random.uniform(constants.PHI_MIN, constants.PHI_MAX, self.num)
+            self.df["theta"] = 2 * np.arctan(np.exp(-self.df["eta"]))
 
-        # Project the lines to each layer
+        # use uniform x,y distribution
+        else:
+
+            # generate more than necessary (some will fall outside bounds)
+            x_mid = np.random.uniform(-1100, 1100, 10 * self.num)
+            y_mid = np.random.uniform(900, 4800, 10 * self.num)
+            z_mid = constants.ZMID
+            phi = np.arctan2(y_mid, x_mid)
+            indices = (phi > constants.PHI_MIN) & (phi < constants.PHI_MAX)
+
+            # trim to num
+            x_mid = x_mid[indices][:self.num]
+            y_mid = y_mid[indices][:self.num]
+            self.df["phi"] = phi[indices][:self.num]
+
+            # calculate
+            r = np.sqrt(x_mid**2 + y_mid**2)
+            self.df["theta"] = np.arctan2(r, z_mid)
+            self.df["eta"] = -np.log(np.tan(self.df["theta"] / 2))
+
+        # Project the lines to each layer, and smear
         logger.info("Projecting lines to each layer")
         for layer in range(constants.LAYERS):
             self.df[f"x_{layer}"] = constants.ZS[layer] * np.tan(self.df["theta"]) * np.cos(self.df["phi"]) + np.random.normal(0, smear, self.num)
